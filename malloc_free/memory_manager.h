0a64f79a002185eb55283a4b4f64723d290ceb32
HungXum <hxuuaanng@qq.com>
2019-06-17 22:43:31 +0800

    搞清楚了used malloc_block的内存结构,需要分析free后pre_size和size的low-bit标志位
    Signed-off-by: HungXum <hxuuaanng@qq.com>

--- malloc_free/memory_manager.h
+++ malloc_free/memory_manager.h
@@ -43,6 +43,13 @@ struct malloc_block
 {
     SIZE_SZ pre_size;
     SIZE_SZ size;
+    struct malloc_chunk* fd;         /* double links -- used only if free. */
+    struct malloc_chunk* bk;
+
+    /* Only used for large blocks: pointer to next larger size.  */
+    struct malloc_chunk* fd_nextsize; /* double links -- used only if free. */
+    struct malloc_chunk* bk_nextsize;
+
 };
 
 typedef struct malloc_block* mchunkptr;
@@ -58,6 +65,18 @@ typedef struct malloc_block* mchunkptr;
 #define pre_chunk(p) ((mchunkptr)((char*)(p) - ((p)->pre_size)))
 #define mem2chunk(mem) ((mchunkptr)((char*)mem - 2 * sizeof(SIZE_SZ)))
 
+#define MIN_CHUNK_SIZE        (offsetof(struct malloc_chunk, fd_nextsize))
+
+#define MALLOC_ALIGNMENT       (2 *SIZE_SZ)
+#define MALLOC_ALIGN_MASK      (MALLOC_ALIGNMENT - 1)
+
+#define MINSIZE  \
+    (unsigned long)(((MIN_CHUNK_SIZE+MALLOC_ALIGN_MASK) & ~MALLOC_ALIGN_MASK))
+
+#define request2size(req)                                  \
+    (((req) + SIZE_SZ + MALLOC_ALIGN_MASK < MINSIZE)  ?    \        
+    MINSIZE :                                              \       
+    ((req) + SIZE_SZ + MALLOC_ALIGN_MASK) & ~MALLOC_ALIGN_MASK)
 
 void malloc_mem_control_block()
 {
@@ -77,7 +96,11 @@ void malloc_mem_control_block()
     printf("-----------------------------\n");
 
     //前面的mem_control_block或者是malloc_block应该是8个字节空间,但是输出的第一个字节是 size(开辟的size+sizeof(mem_control_block)) + isFree;例如size = 104,第一个字节输出是0x71，104 + 对齐8的字节 = 0x70;对齐规则:(size + sizeof(mem_control_block) + 对其的字节)%16 == 0(64位系统)
-    //但是实验得出我的电脑是8字节对齐,因为申请121个字节的空间,如果是16字节对齐,应该得出整个block的size = 144,但是结果是136;而且很奇怪的一个点是block的header只有size = 8字节,并没有glibc中的pre_size??????
+    
+
+    //问题:而且很奇怪的一个点是block的header只有size = 8字节,并没有glibc中的pre_size??????
+    //答案:当一个chunk为空闲时，至少要有prev_size、size、fd和bk四个参数，因此MINSIZE就代表了这四个参数需要占用的内存大小；而当一个chunk被使用时，prev_size可能会被前一个chunk用来存储，fd和bk也会被当作内存存储数据，因此当chunk被使用时，只剩下了size参数需要设置，request2size中的SIZE_SZ就是INTERNAL_SIZE_T类型的大小，因此至少需要req+SIZE_SZ的内存大小。MALLOC_ALIGN_MASK用来对齐，因此request2size就计算出了所需的chunk的大小
+    //从request2size(req)可以看出整个block的大小是(size + size_t + 补齐16字节)
     //--------------------
     //.     .            .
     //.     .            .
@@ -100,19 +123,25 @@ void malloc_mem_control_block()
     void* ptr4 = malloc(200 * 1024);
     printf("ptr4 = %#x\n", ptr4);
     memset(ptr4, 0x04, 200 * 1024);
+    cur_chunk_data(ptr4);
+    printf("-----------------------------\n");
 
-
-    //第一个curBrk和最后的curBrk相差0x21000, 33 * 4KB,则malloc申请内存时,系统会一次行映射33个内存页
+    // 第一个curBrk和最后的curBrk相差0x21000, 33 * 4KB,则malloc申请内存时,系统会一次行映射33个内存页
     // curBrk = sbrk(0);
     // printf("curBrk = %#x\n", curBrk);
 
     free(ptr1);
     free(ptr2);
     free(ptr3);
-    // free(ptr4);
+    free(ptr4);
     // printAddrData1Byte((int*)ptr4 - 2, ptr4); //printf:0xffffffa0 0 0 0 0 0 0 0
 
     //进程结束后,页表被删除了,即被映射的虚拟内存和物理内存的关系不存在了,所有的资源也被回收了,内存泄露的问题也就不存在了,所以内存泄露最大的担心点是出现在长时间运行程序,比如守护进程,服务等.
+
+    printf("-------------------free----------------\n");
+    cur_chunk_data(ptr1);
+    cur_chunk_data(ptr2);
+    cur_chunk_data(ptr3);
 }
 
 void printAddrData1Byte(void* startAddr, void* endAddr)
@@ -140,12 +169,14 @@ void printMCB(void* ptr)
 void cur_chunk_data(void* mem)
 {
     mchunkptr pchunk = mem2chunk(mem);
-    printf("mem = %#x, pchunk = %#x,size before chunksiez = %d, size = %zu\n", mem, pchunk, pchunk->size, chunksize(pchunk));
+    printf("mem = %#x, pchunk = %#x,size before chunksize = %d, size = %zu,, pre_size = %d\n", mem, pchunk, pchunk->size, chunksize(pchunk), pchunk->pre_size);
 }
 
 void pre_chunk_data(void* mem)
 {
     mchunkptr pchunk = mem2chunk(mem);
+    //If prev_inuse is set for any given chunk, then you CANNOT determine the size of the previous chunk, and might even get a memory addressing fault when trying to do so.
+    //那官方的malloc_block结构的各个字段什么时候发挥作用呢???
     mchunkptr prechunk = pre_chunk(pchunk);
     printf("prechunk = %#x, prechunk->size = %zu\n", prechunk, chunksize(prechunk));
 }
